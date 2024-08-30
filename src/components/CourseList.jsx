314c849b7e251e2f27de0fc32fa2ef23e810de2e
Tashdik <s2110477153@ru.ac.bd>
2024-08-30 11:55:28 +0600

    details page design complete

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -14,29 +14,40 @@ const CourseList = () => {
     useEffect(() => {
         dispatch(fetchCourses(searchTerm));
     }, [dispatch, searchTerm]);
+    
 
 
     return (
-        <div>
-            Total courses {courses.length}
-            <input
-                type="text"
-                placeholder="Search by course name or instructor"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className="mb-4 p-2 border border-gray-300 rounded w-full"
-            />
-            <div className='h-40 border-2 p-2 overflow-y-auto'>
-                {
-                    courses.map(course => (
-                        <Link to={`/course/${course._id}`} key={course._id}>
-                            <div className='bg-slate-300 my-2 p-2'>
-                                <p>{course.name}</p>
-                                <p>{course.instructor}</p>
+        <div className="bg-gray-100 text-gray-900">
+            <div className="container grid grid-cols-12 mx-auto">
+                <div className="flex flex-col justify-center col-span-12 align-middle bg-gray-300 bg-no-repeat bg-cover lg:col-span-6 ">
+                    <div className="flex flex-col items-center p-8 py-12 text-center text-gray-800">
+                        <span>12 June</span>
+                        <h1 className="py-4 text-5xl font-bold">Lorem, ipsum dolor sit amet consectetur adipisicing.</h1>
+                        <p className="pb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, a!</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-7 h-7">
+                            <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                        </svg>
+                    </div>
+                </div>
+                <div className="flex flex-col col-span-12 p-6 divide-y lg:col-span-6 lg:p-10 divide-gray-300  h-screen overflow-y-auto">
+                    {
+                        courses.map(course => (
+                            <div key={course._id} className="pt-6 pb-4 space-y-2">
+                                <span>{course?.location}</span>
+                                <h1 className="text-3xl font-bold">{course?.name}</h1>
+                                <p>{course?.instructor}</p>
+                                <Link to={`/course/${course._id}`} rel="noopener noreferrer" href="#" className="inline-flex items-center py-2 space-x-2 text-sm text-violet-600">
+                                    <span>Details</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
+                                        <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                                    </svg>
+                                </Link>
                             </div>
-                        </Link>
-                    ))
-                }
+                        ))
+                    }
+
+                </div>
             </div>
         </div>
     );
--- src/pages/details/CourseDetails.jsx
+++ src/pages/details/CourseDetails.jsx
@@ -6,16 +6,49 @@ import { fetchCourseDetails } from '../../features/coursesSlice';
 const CourseDetails = () => {
     const { id } = useParams();
     const dispatch = useDispatch();
-    const courseDetails = useSelector((state) => state.courses.details);
+    const { price, syllabus, prerequisites, location, schedule, duration, thumbnail, enrollmentStatus, description, instructor, name, _id } = useSelector((state) => state.courses.details);
 
     useEffect(() => {
         dispatch(fetchCourseDetails(id));
     }, [id, dispatch]);
 
     return (
-        <div>
-            <h1>{courseDetails?.name}</h1>
-            <p>{courseDetails?.description}</p>
+        <div className="p-5 mx-auto sm:p-10 md:p-16 bg-gray-100 text-gray-800">
+            <div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
+                <img src="https://img.freepik.com/free-vector/hand-drawn-flat-design-sql-illustration_23-2149243381.jpg?uid=R141467370&ga=GA1.1.221420494.1715600354&semt=ais_hybrid" alt="" className="w-full h-60 sm:h-96 bg-gray-500" />
+                <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md bg-gray-50">
+                    <div className="space-y-2 grid grid-cols-6">
+                        <div className='col-span-5'>
+                            <h1 rel="noopener noreferrer" className="inline-block text-2xl font-semibold sm:text-3xl">{name}</h1>
+                            <p className="text-xs text-gray-600">By {instructor}</p>
+                        </div>
+                        <div>
+                            <h3 className='py-1 px-2 rounded-md bg-green-400 mx-4 text-white font-semibold'>{location}</h3>
+                        </div>
+                    </div>
+                    <div className="text-gray-800">
+                        <p>{description}</p>
+                    </div>
+                    <div className='space-y-3'>
+                        <h3 className='font-medium'>Status : <span>{enrollmentStatus}</span></h3>
+                        <h3 className='font-medium'>Duration : <span>{duration}</span></h3>
+                        <h3 className='font-medium'>Prerequisite : {prerequisites?.map(i => (<span key={i}>{i} </span>))}</h3>
+                        <h3>
+                            <div className='grid grid-cols-1 lg:grid-cols-2 gap-2'>
+                                {syllabus?.map((item, idx) => (
+                                    <div key={idx} className='border-2 rounded-md p-1 border-gray-700'>
+                                        <h3 className='font-medium'><span className='font-bold'>Week :</span> {item.week}</h3>
+                                        <h3 className='font-medium'><span className='font-bold'>Topic :</span> {item.topic}</h3>
+                                        <p className='font-medium'><span className='font-bold'>Content :</span> {item.content}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </h3>
+                        <h3 className='font-medium'>Schedule : <span>{schedule}</span></h3>
+                        <h3 className='font-medium'>Course Fee : $<span>{price}</span></h3>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
